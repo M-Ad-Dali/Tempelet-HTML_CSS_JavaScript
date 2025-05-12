9fc7b70f3f18f1e3bef4c56d8e62b5e144ab3944
Moka-Ad-Dali <mohammed.addali@gmail.com>
2025-05-13 01:09:35 +0300

    switch_random_bacground_imgs

--- js/master.js
+++ js/master.js
@@ -1,3 +1,32 @@
+// Check If There's Local Storage Color Option
+let mainColors = localStorage.getItem("color_option");
+
+if (mainColors !== null) {
+  // console.log("Local Stotorage Is Not  Emoty You Can Set It On Root Now");
+  // console.log(localStorage.getItem("color_option"));
+
+  document.documentElement.style.setProperty('--main-color', localStorage.getItem("color_option"));
+
+  // Remove Active Class From All Colors List Itme
+  document.querySelectorAll(".colors-list li").forEach(element => {
+    element.classList.remove("active");
+
+    // Add Active Class On Element With Data-Color === Local Storage Itne
+    if (element.dataset.color === mainColors) {
+
+      // Add Active Class
+      element.classList.add("active");
+    }
+  });
+}
+
+// Rndom Background Option
+let backgroundOption = true;
+
+// Variable To Control The Bacground Interval
+let backgroundInterval;
+
+
 // Toggle Spin Class On Icon
 document.querySelector(".toggle-settings .gear").onclick = function () {
 
@@ -18,14 +47,51 @@ colorsLi.forEach(li => {
   // Click On Every List Itme
   li.addEventListener("click", (e) => {
 
-    // Remove Class Active
-    // li.classList.remove("active");
+    // Remove Active Class From All Colors List Itme
+    colorsLi.forEach(li => {
+    li.classList.remove("active");
+    });
 
     // Add Class Active
-    // li.classList.add("active");
+    li.classList.add("active");
 
-    // Set Color On Rot
+    // Set Color On Root
     document.documentElement.style.setProperty('--main-color', e.target.dataset.color);
+
+    // Set Color On Itme
+    localStorage.setItem("color_option", e.target.dataset.color);
+
+  });
+});
+
+// Switch Random Bacground Option
+const randomBackEl = document.querySelectorAll(".random-backgrounds span");
+
+// Loop On All List Itmes
+randomBackEl.forEach(span => {
+  
+  // Click On Every Span
+  span.addEventListener("click", (e) => {
+
+    // Remove Active Class From All Colors List Itme
+    randomBackEl.forEach(span => {
+    span.classList.remove("active");
+    });
+
+    // Add Class Active
+    span.classList.add("active");
+
+    if (e.target.dataset.background === 'yes') {
+
+      backgroundOption = true;
+      randomizeImgs();
+
+    } else {
+
+      backgroundOption = false;
+      clearInterval(backgroundInterval);
+
+    }
   });
 });
 
@@ -35,12 +101,23 @@ let landingPage = document.querySelector(".landing");
 // Get Array Of Imgs
 let imgsArray = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
 
-// Timeporary Function For Landing
-setInterval(() => {
+// Function To Randomize Imgs
+function randomizeImgs() {
+
+  if (backgroundOption === true) 
+
+    // Timeporary Function For Landing
+backgroundInterval = setInterval(() => {
 
   // Get Rnadom Number
   let randomNumber = Math.floor(Math.random() * imgsArray.length);
 
   // Change Background Image Url
   landingPage.style.backgroundImage = `url('imgs/landing/${imgsArray[randomNumber]}')`;
-}, 8000);
+}, 8000);{
+
+  }
+
+}
+
+randomizeImgs();
