8d7d6cc225b1fcc8bdd6ce43f402553f18610c19
Moka-Ad-Dali <mohammed.addali@gmail.com>
2025-05-15 19:02:19 +0300

    gallery

--- js/master.js
+++ js/master.js
@@ -170,29 +170,34 @@ randomizeImgs();
 // Select Skills Selector
 let skillsOldYemen = document.querySelector(".old-yemen");
 
-window.onscroll = function () {
+window.onscroll =  function () {
 
   // Skills Offset Top
   let skillsOffsetTop = skillsOldYemen.offsetTop; // [الجزء اللي فوق السكشن اللي ندور عليه يعرضلك كم هو بالبكسل]
 
+  
   // Skills Outer Height
   let skillsOuterHeight = skillsOldYemen.offsetHeight; // [تحسب هيت وبادنج وكل شيء لسكشن اللي انا فيه بالبكسل]
-
+  
   // Window Height
   let windowHeight = this.innerHeight; // [الهايت حق الويندو كامل]
-
+  
   // Window ScrollTop
   let windowScrollTop = this.pageYOffset; // [الجزء اللي انا اعمل فيه سكرول كم بالبكسل]
 
-  if (windowScrollTop > (skillsOffsetTop + skillsOuterHeight - windowHeight)) { // [اذا كان الجزء اللي اعمل فيه سكرول اكبر من (الجزء اللي فوق الكشين + اهايت للسكشن كامل - هايت الويندو كامل)] [هذي المعادلة أول ما ادخل لسكشن المطلوب يتم عمل حدث]
+  // console.log(windowScrollTop)
+  // console.log(skillsOffsetTop + skillsOuterHeight - windowHeight)
+  if (windowScrollTop > (skillsOffsetTop + skillsOuterHeight - windowHeight - 1)) { // [اذا كان الجزء اللي اعمل فيه سكرول اكبر من (الجزء اللي فوق الكشين + اهايت للسكشن كامل - هايت الويندو كامل)] [هذي المعادلة أول ما ادخل لسكشن المطلوب يتم عمل حدث]
 
     // this.console.log('Skills Section Reached'); // [لما يوصل لسكشن المطلوب اعرض الريالة هذي بالكنسل]
 
     let allSkills = document.querySelectorAll(".skill-box .skill-progress span");
+    // console.log(allSkills);
 
     allSkills.forEach(skill => {
 
       skill.style.width = skill.dataset.progress; // [[dataset.progriss = data-progress="96%"] عند التعامل مع الداتا ست تكتب الكلمة اللتي بعد الاتا ست مثل]
+      
 
     });
 
@@ -200,3 +205,86 @@ window.onscroll = function () {
 
   // console.log(windowHeight);
 };
+
+// Create Popup With The Image
+let ourGallery = document.querySelectorAll(".gallery img");
+
+ourGallery.forEach(img => {
+
+  img.addEventListener('click', (e) => {
+
+    // Create Overlay Element
+    let overlay = document.createElement("div");
+
+    // Add Class To Overlay
+    overlay.className = 'popup-overlay';
+
+    // Append Overlay To The Body
+    document.body.appendChild(overlay)
+
+    // Create The Popup Box
+    let popupBox = document.createElement("div");
+
+    // Add Class To The Popup Box
+    popupBox.className = 'popup-box'
+
+    if (img.alt !== null) {
+
+      // Create Heading
+      let imgHeading = document.createElement("h3");
+
+      // Create text For Heading
+      let imgText = document.createTextNode(img.alt);
+
+      // Append The Text To The Heading
+      imgHeading.appendChild(imgText);
+
+      // Append The Heading TO The Popup Box
+      popupBox.appendChild(imgHeading);
+
+    }
+
+    // Create The Image
+    let popupImage = document.createElement("img");
+
+    // Set Image Source
+    popupImage.src = img.src;
+
+    // Add Image To Popup Box
+    popupBox.appendChild(popupImage);
+
+    // Append The Popup Box To Body
+    document.body.appendChild(popupBox)
+
+    // Create  The. Close Span
+    let closeButton = document.createElement("span");
+
+    // Create The.Close Button Text
+    let closeButtonText = document.createTextNode("X");
+
+    // Append Text To Close Button
+    closeButton.appendChild(closeButtonText);
+
+    // Add Class To Close.Button
+    closeButton.className = 'close-button';
+
+    // Add Close Button To The Popup Box
+    popupBox.appendChild(closeButton);
+
+  });
+
+});
+
+// -Close Popup
+  document.addEventListener("click", function (e) {
+
+  if (e.target.className == 'close-button') {
+
+  // Remove The Current Popup
+  e.target.parentNode.remove();
+
+  // Remove Overlay
+  document.querySelector(".popup-overlay").remove();
+
+  };
+});
